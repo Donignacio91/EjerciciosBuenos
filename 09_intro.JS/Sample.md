337bd4ba04295a589429fc2e15751a29a71a90ee
Donignacio <naxo10011991@gmail.com>
2019-05-22 21:25:59 +0200

    jaleal css

---
+++ 09_intro.JS/Sample.md
@@ -0,0 +1,34 @@
+# curso web 2019 Tajamar <!-- el formato es markdown con esto definiremos Titulos listas parrafos , hiperenlaces y bloques https://markdown.es/ -->
+
+# h1 titulo principal#
+##  h2 titulo de sección titulo 2 2º titulo ##
+
+Ejemplo de un párrafo con **negrita** y _cursiva_
+
+Ejemplo de un código
+
+```js
+let a = 'pepe'
+
+```
+
+Ejemplo de código en linea; `let b = '32'`
+
+Ejemplo de listas 
+- uno
+- dos
+    - sub dos
+        - sub sub dos
+-  tres
+
+
+![van](../07_ElementosWeb/assets/vanlife.jpg)
+
+[markdown](https://markdown.es/)
+
+
+Column A | Column B | Column C
+---------|----------|---------
+ A1 | B1 | C1
+ A2 | B2 | C2
+ A3 | B3 | C3
---
+++ 09_intro.JS/app.js
@@ -0,0 +1,68 @@
+/* console es un objeto que se encarga de mostrar la info */
+console.log("Hola gente!")
+
+//datos
+
+ //Tipos primitivos
+20   //tipo number
+'alejandro'  //tipo string
+true  //tipo boolean
+undefined // tipo undefine
+
+/* //Tipos complejos o referenciados
+{ //coleccion de propiedades
+    nombre:'Pepe'
+    edad:'27'
+}  */
+
+//los lenguajes pueden ser  tipado debil o fuerte
+
+//los lenguajes pueden ser tipado estático (variables de tipo definido fijo) o dinámico no fijo 
+
+//JavaScrip es débil y dinámico
+
+/* //En Java C# el tipado es fuerte y estático
+a :int //tipo int sin valor
+a = 3 
+a = 'pepe' //error de tipos */
+
+
+let a 
+console.log(a)
+console.log(typeof a) //me devuelve undefined
+
+a = 2
+console.log(a)
+console.log(typeof a) //me devuelve number
+
+a = 25
+console.log(a)
+console.log(typeof a) //me devuelve number
+
+a = 'Pepe'
+console.log(a)
+console.log(typeof a)//me devuelve string  console.log (type of) me dice de que tipo es una variable 
+
+ a = false 
+console.log(a)
+console.log(typeof a) //me devuelveboolean
+// para comporbarlo sobre la carpeta en el terminal ejecuto node .\app.js
+
+let precio =  45.00
+let producto = 'Gafas'
+console.log(a)
+console.log(typeof a)
+
+//uso normal de datos y variables
+
+let precio =  45.00;
+let producto = 'Gafas'
+console.log(a)
+console.log(typeof a)  
+
+
+a = [{nombre:'Pepe',edad:27},
+{nombre = 'pepa',edad: 35},
+{nombre = 'pepin',edad: 15}]
+console.log(a)
+console.log(typeof a)  
