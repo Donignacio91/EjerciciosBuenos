e4815ff5886936af74ea226ce49050957f971f4b
donignacio91 <naxo10011991@gmail.com>
2019-07-19 12:54:50 +0200

    x

--- Web_js/06_salir/js/app.js
+++ Web_js/06_salir/js/app.js
@@ -1,13 +1,57 @@
 export function app() {
     console.log('app runs')
 
-     let btnCancel = document.querySelector('#btn-cancel')
+    let btnCancel = document.querySelector('#btn-cancel')
     let btnSalir = document.querySelector('#btn-salir')
     let dlgCancelar =document.querySelector('#dlg-cancelar')
     let handler   // la dec
-
+    let backTime = document.querySelector('#back-time')
+    let outRun = document.querySelector('#out-run')
+    let btnPlay = document.querySelector('#btn-play')
+    let outShow =  document.querySelector('#out-show')
     btnSalir.addEventListener('click', onClick)
     btnCancel.addEventListener('click', onClick2)
+    btnPlay.addEventListener('click', run)
+// no les paso eventono se por que ni como lo definiria
+    function run(ev) { 
+        let msj = 'Mis coches de ensueño:'
+        outShow.textContent = msj
+        let aCoches=[ 'Alfa Romeo, ','Lancia, ', 'Ferrari, ', 'BMW, ', 'Ford' ]
+       
+        /* setTimeout(()=>{
+
+            for (let i = 0; i < aCoches.length; i++) {
+            const item = aCoches[i];
+          console.log(item)
+          outRun.textContent += item
+        }
+        },2000)  */
+
+        /*  setTimeout(()=>{
+            aCoches.forEach(item => {
+                outRun.textContent += item
+            });
+        },2000)  */
+       /*    */
+         
+       
+      
+       
+           let time=  setInterval(()=>{
+
+                for (let i = 0; i < aCoches.length; i++) {
+                let item = aCoches[i];
+            
+              outRun.textContent = item
+            }
+            },1500)
+
+        setTimeout(time,1000)
+
+
+       // outRun.textContent
+        
+    }
 
 
     function onClick(ev) {
@@ -15,8 +59,11 @@ export function app() {
         handler = setTimeout(() => {
             location.assign(url)
         }, 5000)
-       
-        dlgCancelar.showModal()
+        let i = 5
+        setInterval( () => {
+            backTime.textContent = --i
+        },1000)
+        dlgCancelar.showModal()//para ver el dialogo
     
       
         //outSal.value = msj //solo vale para elementsOutputs
--- Web_js/intervalos.js
+++ Web_js/intervalos.js
@@ -7,8 +7,22 @@ setTimeout(( ) => {
     clearTimeout(handler)   // le digo que me borre la cola de eventos en dos segundos como el otro tarda 4 en salir la eliminade la cola de eventos 
 },2000 )
  */
-let i = 0
+/* let i = 0
 setInterval( () => {
     console.log(++i)
-},100)  //realiza esto hasta que alguien lo pare
+},1000)  //realiza esto hasta que alguien lo pare
+ */
+//let aCoches=['old Ferrari´s', 'alfa romeo' ]
+//aCoches.forEach(element => {console.log(element)});
 
+let aCoches=[ 'Alfa Romeo, ','Lancia, ', 'Ferrari, ', 'BMW, ', 'Ford' ]
+       
+ setTimeout(()=>{
+
+    for (let i = 0; i < aCoches.length; i++) {
+    let item = aCoches[i];
+  console.log(item)
+  //outRun.textContent += item
+}
+},2000) 
+console.log(aCoches)
