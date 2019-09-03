0dbf3c91f92d92004ac2eec383902bb5a8cd6914
Donignacio91 <CursosTardes@tic.loc>
2019-09-03 21:19:24 +0200

    xvbxcb

--- Web_js/21_Async_fech/js/app.js
+++ Web_js/21_Async_fech/js/app.js
@@ -7,76 +7,47 @@ export function app() {
     let inputId = document.querySelector('#input-Id')
     let btnBuscar =document.querySelector('#btn-Buscar')
     let btnBorrar = document.querySelector('#btn-Borrar') 
-
-    
     let outputNombre = document.querySelector('#span_saludo')
     
-
-
     //Manejadores
-
     btnBorrar.addEventListener('click',onClickBorrar) 
-    btnBuscar.addEventListener('click', onClickBuscar)
+    btnBuscar.addEventListener('click', onClickBuscar2017)
    
-    
-
-    //Inicializacioón de valores 
-    /*  let user = localStorage.getItem(store)//este solo quiere saber donde bscar 
-    if (user) {
-        inputNombre.value = store
-        outputNombre.innerHTML = inputNombre.value
-    }   */
-
-    //Funciones manejadoras de eventos
-
-
-
-
     function onClickBorrar(ev) {
         inputId.value = ''
     }
-
+ 
     function onClickBuscar(ev){
 
-        if(inputId.value < 0 || inputId.value > 10){ 
-            outputNombre.innerHTML =', registro no valido solo del 1 al 10'
-            return
-        }
         let url = userURL + '/' + inputId.value 
+        fetch(url)  // una promsea e un objeto que devuelve valores pero en un futuro  
+        .then(response=>{ 
+            if(response.status == 200){
+            return  response.json()
+        }
+            throw(new Error(response.status))
 
-        ajax('get', url, leerDatos)//funcion que yo programo para independizar y tnerla disponible siempre recibe la wl metodo la url y la funcion callback en este caso la llamo leer datos pero puede ser cualquier otra funcion que quiwera segun lo que quirea hacer 
-}
+            
+        }).then( (data) => {
+          outputNombre.innerHTML = data.username
+        }) //funcion para cuando salga bien la promise
+        .catch((error)=>{
+            outputNombre.innerHTML ='liada parda problemon fallo conexión ' + error
+        }) //funcion para cuando salga mal la promise
 
-function ajax (metodo , url , callback) {
-    
- http.addEventListener('readystatechange', ()=>{callback(http)}) 
- http.open(metodo, url) 
- http.send() 
-}
- //AJAX
- 
- 
-//funcion del evento AJAX 
-function leerDatos(http) {
+    } 
 
-  /*   if (http.readyState == 4 && http.status == 200) {
-    let data = JSON.parse(http.responseText) // es lo que manda el server en string si es en json text si es en xml responsexml 
-    //readistate 4 me dice si ha terminado y el status me dice como a ido el 200 es todo bien 404 sertia un fallo  
- console.log(http.responseText)// los datos enviados por el servidor(api)
-
- outputNombre.innerHTML = data.username
-} else if (http.readyState == 4 ) {
-    //outputNombre.innerHTML ='liada parda problemon'
-     location.assign('./error.html') //redireccion desde java 
-} */
-if (http.readyState == 4){
-    if(http.status == 200){
-        let data = JSON.parse(http.responseText)
-        outputNombre.innerHTML = data.username
-    } else {
-        //outputNombre.innerHTML ='liada parda problemon'
-        location.assign('./error.html') //redireccion desde java 
-    }
-}
-}
-}
+    async function onClickBuscar2017(ev) {
+        let url = userURL + '/' + inputId.value 
+        try {
+            let response = await fetch(url)
+            if(response.status == 200){
+               let data = await response.json()
+                outputNombre.innerHTML = data.username
+        } else {
+            throw(new Error(response.status))}
+        } catch (error) {
+        
+            outputNombre.innerHTML ='liada parda problemon fallo conexión ' + error
+        }
+        }}
