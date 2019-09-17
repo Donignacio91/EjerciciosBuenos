d9a04c81eb401c7c42125c5b663743cc32c3c830
Donignacio91 <CursosTardes@tic.loc>
2019-09-17 18:23:18 +0200

    oleole

---
+++ http/index.js
@@ -0,0 +1,6 @@
+const http = required('http')
+const port = 8080
+
+http.createdServer((request , response )=>{
+request.
+})
---
+++ phpCarpet/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio php</title>
+</head>
+<?php
+ $title="Hola mundo";
+?>
+<body>
+    <header>
+        <h1> 
+        <?= $title
+             /* echo $title lo mismo */
+        ?>        
+    </h1>
+    </header>
+    
+</body>
+</html>
