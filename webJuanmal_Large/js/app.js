a10ed8eafcc7eaa55116341318cbf6e477a30613
Donignacio91 <Naxo10011991@gmail.com>
2019-09-24 12:24:41 +0200

    large

---
+++ webJuanmal_Large/js/app.js
@@ -0,0 +1,23 @@
+export function app(){
+    console.log('app runs')
+    //Nodos
+    let enlaz = document.querySelector('.redirec')
+    let galery = document.querySelector('.seccionContenido')
+    //Manejadores
+    enlaz.addEventListener('mousemove', onEnlaz)
+    enlaz.addEventListener('click', onClick)
+    galery.addEventListener('mouseenter',cambia)
+    //funciones 
+   function  onEnlaz(ev){ 
+       enlaz.classList.add("cursor")
+    }
+    function onClick(ev) {
+        console.log(ev)
+        window.location="https://www.google.es/maps/place/EB+Racing/@40.5325473,-3.4865579,18.25z/data=!4m5!3m4!1s0xd423465a6cfd643:0x8d41b48a04615d85!8m2!3d40.532641!4d-3.4861538";
+    }
+
+
+    
+
+
+} 
