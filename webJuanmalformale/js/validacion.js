6c0a3abe5d9e0702addc0e6a7fbcf2d546dc6b2b
Donignacio91 <Naxo10011991@gmail.com>
2019-09-26 13:48:08 +0200

    +form

---
+++ webJuanmalformale/js/validacion.js
@@ -0,0 +1,45 @@
+import { comprobarDNI } from './helper.js'
+
+export function validacion() {
+
+    let userName = document.querySelector('#userName')
+    // userName.addEventListener('input', onUserInput)
+    userName.addEventListener('blur', onUserInput)
+    console.dir(userName)
+
+    function onUserInput(ev) {
+        let nodo = ev.target
+        console.dir(nodo)
+        // if (!nodo.validity.valid) { 
+           if( !nodo.checkValidity()) {
+            nodo.nextElementSibling.innerHTML = 
+                nodo.validationMessage
+            nodo.nextElementSibling.hidden = false    
+        }
+    }
+}
+
+export function validacionFinal(nodos) {
+    console.dir(nodos)
+    let r = false
+    nodos.some(nodo => {
+        if (nodo.name === 'dni') { // nodo del dni
+            nodo.setCustomValidity('')
+            if (nodo.value && !comprobarDNI(nodo.value)) {
+                nodo.setCustomValidity('Letra del DNI incorrecta') 
+            }    
+        }
+        if( !nodo.checkValidity()) {
+            nodo.parentNode.nextElementSibling.innerHTML = 
+                nodo.validationMessage
+            nodo.parentNode.nextElementSibling.hidden = false
+            r = false  
+            return true  
+        } else {
+            r = true
+            nodo.parentNode.nextElementSibling.hidden = true
+        }
+    });
+
+    return r
+}
